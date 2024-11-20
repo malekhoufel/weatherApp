e8094b6ca0f4718c21924ec952fc4876d3c56711
Malek Houfel <abdelmalek.houfel@sinay.fr>
2024-11-20 15:46:12 +0100

    tp4

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="malekhoufel_weather_app",
+    version="0.1.0",
+    author="Malek",
+    author_email="abdelmalek.houfel@sinay.fr",
+    description="application météo",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/malekhoufel/weatherApp.git",
+    packages=find_packages(),
+    install_requires=["requests"],
+        
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
